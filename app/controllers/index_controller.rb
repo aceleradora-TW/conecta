82108f1754a7704842a833fafd08fc97f0d5643b
tatianeaguirres <aguirres.tati@gmail.com>
2017-05-09 15:58:29 -0300

    #4 <@gmeneguz, @hsangalli> - Adicionar rotas de teste dos elementos do banco

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,40 +1,23 @@
 get "/" do
   @error = []
-
-  ### Exemplo de como Utiliza o Banco de dados
-  # @company = Company.new(name: 'tw', description: 'uma descricao qlqr', size: 'GRANDE')
-  # @location = Location.new(city: "Porto Alegre", number: "123", state: "RS", street: "Av Ipiranga")
-  # if !@company.save
-  #   @company.errors.each do |error|
-  #     @error.push(error)
-  #   end
-  # end
-  # if !@location.save
-  #   @location.errors.each do |error|
-  #        @error.push(error)
-  #   end
-  # end
-  #
-  # @location.institution = @company
-  # @location.save
-  # @locationSozinha = @company.locations
   erb :index
 end
 
-get "/test" do
-  # @error = []
-  # @company = Company.create(name: 'tw', description: 'uma descricao qlqr', size: 'GRANDE')
-  #   @company.save
-  # @type = Type.new(name: "Produtos")
-  # if !@type.save
-  #   @type.errors.each do |error|
-  #     @error.push(error.to_s)
-  #   end
-  #   return "Erro: " + @error.to_s
-  # end
-  # @company.types << @type
-  # @company.save
-  #
-  # return @company.types[0].name
-  #erb :index
+get "/companies" do
+  @companies = Company.all
+  erb :companies
+end
+
+get "/segments" do
+  @segments = Segment.all
+  erb :segments
+end
+
+get "/competences" do
+  @competences = Competence.all
+  erb :competences
+end
+
+get "/import-companies" do
+  require_relative "../../config/importCompany.rb"
 end
