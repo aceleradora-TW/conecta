e0a553fe22494c105b87eb27a1eb471704e3c658
tatianeaguirres <aguirres.tati@gmail.com>
2017-05-10 13:54:31 -0300

    #4 <@gmeneguz, @bernardofelipetto, @mvoliveira, @hsangalli> - Editar a model da company (through resource)

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ require_relative 'institution'
 
 class Company < Institution
   property :size,       String
+  has n, :segments, :through => Resource
   has n, :companyTypes
   has n, :companyAreas
 end
