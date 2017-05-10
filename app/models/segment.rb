ac5718e05939cdd74256f1ad2310fdc86ea77117
tatianeaguirres <aguirres.tati@gmail.com>
2017-05-10 13:56:23 -0300

    #4 <@gmeneguz, @bernardofelipetto, @mvoliveira, @hsangalli> - Editar a cardinalidade da classe Segment

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,7 +1,6 @@
 class Segment
   include DataMapper::Resource
   property :id,         Serial
-  property :name,       String, :required => true
-
-  belongs_to :company, :required => false
+  property :name,       String #, :required => true
+  has n,:companies, :through => Resource
 end
