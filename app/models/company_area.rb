b8e2c04063d8249ed3b78b1a28208d059884bdeb
tatianeaguirres <aguirres.tati@gmail.com>
2017-05-11 17:06:23 -0300

    #4 <@gmeneguz, @hsangalli, @bernardofelipetto> - Revisar/Limpar arquivos da model

--- app/models/company_area.rb
+++ app/models/company_area.rb
@@ -4,5 +4,4 @@ class CompanyArea
   property :id,    Serial
   property :name,  String, :required => true
   has n, :companies, :through => Resource
-  #belongs_to :company, :required => false
 end
--- app/models/competence.rb
+++ app/models/competence.rb
@@ -3,6 +3,5 @@ class Competence
   include DataMapper::Resource
   property :id, Serial
   property :name, Text, :required => true
-  #has 1, :company_area , :required => false
   has n, :companies, :through => Resource
 end
