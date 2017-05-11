5d52f4ed6b16d0102bfea70afd7523ad7996b31c
marlonfutado <matheus.mvo10@gmail.com>
2017-05-11 11:29:59 -0300

    Refatorar teste para index

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -3,15 +3,12 @@ require 'rspec'
 require 'spec_helper'
 
 RSpec.describe App do
-  # RSpec.feature "Static pages", :type => :feature do
   describe "OLA" do
     context "Quando listar todas as empresas" do
       it "deve mostrar na tela Lista de Instituições" do
-
         visit "/"
         puts page.body
         expect(page).to have_content 'pessoal'
-
       end
     end
   end
