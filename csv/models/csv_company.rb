198130cbf5203d4b67540eabdefee472f31e38a5
ACeleradora <you@example.com>
2017-05-12 16:02:06 -0300

    #5<@gmeneguz> - Resolve problemas ao inserir competências no banco.

--- csv/models/csv_company.rb
+++ csv/models/csv_company.rb
@@ -5,8 +5,11 @@ require_relative '../../app/models/competence'
 class CsvCompany
   attr_accessor :name, :size, :description,:segments,:types,:competences
   def initialize
+    @competence_area_array = nil
+    create_competence_area_array()
   end
   def insert_to_db
+
     company = Company.new(name: @name, size: @size)
     @segments.each do |segment|
       segment_object = Segment.first(name: segment)
@@ -23,14 +26,28 @@ class CsvCompany
       company.company_types.push(type_object)
     end
     @competences.each do |competence|
+      #puts competence
       competence_object = Competence.first(name: competence[:name])
       if !competence_object
-        competence_object = Competence.create(name: competence[:name])
+        competence_area = @competence_area_array[competence[:area_number].to_i]
+        competence_object = Competence.create(name: competence[:name], competence_area: competence_area)
       end
       company.competences.push(competence_object)
     end
     company.save
     puts company.company_types.inspect
-
+  end
+  def create_competence_area_array
+    if !@competence_area_array
+      @competence_area_array = [
+        "",
+        CompetenceArea.create(name: "Hardware"),
+        CompetenceArea.create(name: "Software"),
+        CompetenceArea.create(name: "Administração"),
+        CompetenceArea.create(name: "Serviços"),
+        CompetenceArea.create(name: "Soluções Ecológicas"),
+        CompetenceArea.create(name: "Comercial")
+      ]
+    end
   end
 end
