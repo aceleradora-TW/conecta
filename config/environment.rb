eab0ed2b9d147d6d8f9da923f3586b4984506efb
Henrique Sangalli <habyte@gmail.com>
2017-05-16 14:37:24 -0300

    #0<@cschallenberger,@gmeneguz>-Adicionar configuracao Public para rackup(heroku)

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,10 @@ APP_NAME = APP_ROOT.basename.to_s
 
 configure do
   set :root, APP_ROOT.to_path
-  set :public_folder, File.join(APP_ROOT, "public")
   enable :sessions
   set :session_secret, ENV['SESSION_KEY'] || 'rubyrubyruby'
 
   # Move to app/controllers/config.rb
   # set :views, File.join(Sinatra::Application.root, "app", "views")
 end
-puts "AOO ROOT :" + APP_ROOT.to_path
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
