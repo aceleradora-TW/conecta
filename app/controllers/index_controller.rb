ed9e48061fdbaa41af7aea0a3fad2f139a2435e1
Marlon Furtado <marlonfurtado.rs@gmail.com>
2017-05-24 10:19:10 -0300

    #6<@bernardofelipetto,@marlonfurtado,@gmeneguz>-Alterações na busca

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -50,64 +50,66 @@ class IndexController < Controller
   get "/search_all" do
     @value = params[:value]
     @search_type = params[:search_type]
+    @value_sql = "%#{@value.downcase}%"
 
     if @search_type == 'competencia'
       @companies = Company.all
       area_competence
       @companies_searched = []
-      @competences = Competence.all(:name.like => "%#{@value}%")
-      @competences_id =   @competences.map{|c| c.id}
-      @company_competence = CompetenceInstitution.all(conditions: ['competence_id in ? and (competence_value = 4 or competence_value = 5 or competence_value = 6)',@competences_id])
-      @company_competence.each do |single_cmp_competence|
-        local_company = single_cmp_competence.company
-        if !@companies_searched.include? local_company
-          @companies_searched.push(local_company)
+
+      @competences = Competence.all(:conditions => ["lower(name) like ?", @value_sql])
+      if(@competences.length != 0)
+        @competences_id =   @competences.map{|c| c.id}
+        @company_competence = CompetenceInstitution.all(conditions: ['competence_id in ? and (competence_value >= 4)',@competences_id])
+        @company_competence.each do |single_cmp_competence|
+          local_company = single_cmp_competence.company
+          if !@companies_searched.include? local_company
+            @companies_searched.push(local_company)
+          end
         end
       end
-
-
     elsif @search_type == 'segmento'
       @companies = Company.all
       area_competence
 
       @segments_searched = []
 
-      @segments = Segment.all(:name.like => "%#{@value}%")
-      @segments_id = @segments.map{|s| s.id}
-      @company_segments = InstitutionSegment.all(conditions: ['segment_id in ?',@segments_id])
+      @segments = Segment.all(:conditions => ["lower(name) like ?", @value_sql])
 
-      @company_segments.each do |single_segment|
-        local_company = single_segment.company
-        if !@segments_searched.include? local_company
-          @segments_searched.push(local_company)
+      if @segments.length != 0
+        @segments_id = @segments.map{|s| s.id}
+        @company_segments = InstitutionSegment.all(conditions: ['segment_id in ?',@segments_id])
+        @company_segments.each do |single_segment|
+          local_company = single_segment.company
+          if !@segments_searched.include? local_company
+            @segments_searched.push(local_company)
+          end
         end
       end
-
-
     elsif @search_type == 'empresa'
-      @companies = Company.all(:name.like => "%#{@value}%")
+      @companies = Company.all(:conditions => ["lower(name) like ?", @value_sql])
       area_competence
 
     elsif @search_type == 'centro-pesquisa'
-      @research_center = ResearchCenter.all(:name.like => "%#{@value}%")
+      @research_center = ResearchCenter.all(:conditions => ["lower(name) like ?", @value_sql])
 
     elsif @search_type == 'area-pesquisa'
 
       @research_centers = ResearchCenter.all
 
-      @areas = ResearchArea.all(:name.like => "%#{@value}%")
+      @areas = ResearchArea.all(:conditions => ["lower(name) like ?", @value_sql])
 
     elsif @search_type == 'eixo-pesquisa'
 
       @research_centers = ResearchCenter.all
 
-      @fields = ResearchField.all(:name.like => "%#{@value}%")
+      @fields = ResearchField.all(:conditions => ["lower(name) like ?", @value_sql])
 
     elsif @search_type == 'project'
 
       @research_centers = ResearchCenter.all
 
-      @project = ResearchCenter.all(:project.like => "%#{@value}%")
+      @project = ResearchCenter.all(:conditions => ["lower(project) like ?", @value_sql])
 
 
     end
@@ -131,9 +133,9 @@ class IndexController < Controller
       company.competence_institutions.each do |comp_all|
         if filtered_competences.key?(comp_all.competence.competence_area.name)
           competence_area_array = filtered_competences[comp_all.competence.competence_area.name]
-          competence_area_array.push(comp_all.competence.name + " | " + comp_all.competence_value + " | ")
+          competence_area_array.push(comp_all.competence.name + " | " + comp_all.competence_value.to_s + " | ")
         else
-          competence_area_array = [comp_all.competence.name + " | " + comp_all.competence_value + " | "]
+          competence_area_array = [comp_all.competence.name + " | " + comp_all.competence_value.to_s + " | "]
           filtered_competences[comp_all.competence.competence_area.name] = competence_area_array
         end
         @competences_by_company[company] = filtered_competences
