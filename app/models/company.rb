f086175bb9f15b16ede2258353f9d1b26de9c803
Tatiane Aguirres <aguirres.tati@gmail.com>
2017-05-29 14:37:25 -0300

    #4<@tatianeaguirres,@bernardofelipetto>-refatorar controller e criar search_controller

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,4 +6,19 @@ class Company < Institution
   has n, :company_areas, :through => Resource
   has n, :competences, :through => :competece_institutions
   has n, :competence_institutions
+
+  def grouped_competences
+    filtered_competences = {}
+    competence_institutions.each do |comp_all|
+      if filtered_competences.key?(comp_all.competence.competence_area.name)
+        competence_area_array = filtered_competences[comp_all.competence.competence_area.name]
+        competence_area_array.push(comp_all.competence.name + " | " + comp_all.competence_value.to_s + " | ")
+      else
+        competence_area_array = [comp_all.competence.name + " | " + comp_all.competence_value.to_s + " | "]
+        filtered_competences[comp_all.competence.competence_area.name] = competence_area_array
+      end
+    end
+    return filtered_competences
+  end
+
 end
---
+++ app/services/search_service.rb
@@ -0,0 +1,47 @@
+class SearchService
+
+  def find_by_competence competence_name
+    @competences = Competence.all(:conditions => ["lower(name) like ?", competence_name])
+  end
+
+  def find_related_competence_institutions compenteces
+    competences_id = compenteces.map { |c| c.id }
+    @company_competence = CompetenceInstitution.all(conditions: ['competence_id in ? and (competence_value >= 3)', competences_id])
+  end
+
+  def find_by_segment segment_name
+    @segments = Segment.all(:conditions => ["lower(name) like ?", segment_name])
+  end
+
+  def find_related_segment segments
+    segments_id = segments.map { |s| s.id }
+    @company_segments = InstitutionSegment.all(conditions: ['segment_id in ?', segments_id])
+  end
+
+  def find_by_company companies
+    @companies = Company.all(:conditions => ["lower(name) like ?", companies])
+  end
+
+  def find_by_research_center research_centers
+    @research_center = ResearchCenter.all(:conditions => ["lower(name) like ?", research_centers])
+  end
+
+  def find_by_research_area research_areas
+    @areas = ResearchArea.all(:conditions => ["lower(name) like ?", research_areas])
+  end
+
+  def find_related_area areas
+    research_area_id = areas.map { |a| a.id }
+    @rc_area = InstitutionResearchArea.all(conditions: ['research_area_id in ?', research_area_id])
+  end
+
+  def find_by_research_field research_fields
+    @fields = ResearchField.all(:conditions => ["lower(name) like ?", research_fields])
+  end
+
+  def find_related_field fields
+    research_field_id = fields.map { |f| f.id }
+    @rc_field = InstitutionResearchField.all(conditions: ['research_field_id in ?', research_field_id])
+  end
+
+end
