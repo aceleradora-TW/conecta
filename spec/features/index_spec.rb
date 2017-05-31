986bfdffd1b070f64fc3ea0c1a402dec1a6e77db
Tatiane Aguirres <aguirres.tati@gmail.com>
2017-05-31 11:49:39 -0300

    #8<@tatianeaguirres,@bernardofelipetto>-criar footer

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -10,9 +10,15 @@ RSpec.describe IndexController do
         visit "/"
         expect(page).to have_content 'Conecta'
       end
+
       it "Deve exibir o botão do filtro de busca" do
         visit "/"
         expect(page).to have_css("button", class: 'btn-filter')
+      end
+
+      it "Deve exibir o texto do copyright no footer" do
+        visit "/"
+        expect(page).to have_content "© 2017 Conecta, Todos os direitos deservados."
       end
 
     end
