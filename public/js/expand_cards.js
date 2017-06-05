d3e9777aaea21da019dcf5bdd23859da4d88265a
Gabriela Cardozo <gabrielacardozo09@gmail.com>
2017-06-05 16:23:22 -0300

    #8<@gbrlcardozo,@souzabruna>-Arrumar layout

--- public/js/expand_cards.js
+++ public/js/expand_cards.js
@@ -12,3 +12,18 @@ for(let i = 0; i < items.length; i++) {
     }
   });
 }
+
+const button = document.querySelectorAll('.u-width-medium');
+const cardsecondary = document.querySelectorAll('.CardSecondary');
+for(let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function(){
+    if(cardsecondary[i].style.display == 'block') {
+      cardsecondary[i].style.display = 'none';
+
+    }
+    else{
+      cardsecondary[i].style.display = 'block';
+
+    }
+  });
+}
