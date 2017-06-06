44290b3a8099d78ffb82ce499a18c0b3413b4d29
Gianfranco Meneguz <gmeneguz@gmail.com>
2017-06-06 15:55:04 -0300

    #8<@hsangalli,@gmeneguz>-Alterar Models para listar Estrelas

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,23 @@
 require_relative 'institution'
 class Company < Institution
+  attr_accessor :searched_competences
+
   property :size, String
   has n, :segments, :through => Resource
   has n, :company_types, :through => Resource
   has n, :company_areas, :through => Resource
   has n, :competences, :through => :competece_institutions
   has n, :competence_institutions
+
+  def featured_competences
+    if @searched_competences and @searched_competences.length > 0
+      ordered_competences = @searched_competences.sort{|a,b| b.competence_value_calculated_integer <=> a.competence_value_calculated_integer}
+      return ordered_competences.slice(0,3)
+    else
+      ordered_competences = self.competence_institutions.slice(0,3)
+      ordered_competences = ordered_competences.sort{|a,b| b.competence_value_calculated_integer <=> a.competence_value_calculated_integer}
+    end
+  end
 
   def grouped_competences
     filtered_competences = {}
