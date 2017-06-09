5406b63c1145665de4ead0386f3f19a903f06519
Bernardo Felipetto <be.felipetto@hotmail.com>
2017-06-09 15:50:52 -0300

    #0<@bernardofelipetto,@hsangalli>-Apagar comentarios

--- public/js/expand_cards.js
+++ public/js/expand_cards.js
@@ -11,22 +11,6 @@ for(let i = 0; i < items.length; i++) {
       element.classList.remove("active");
     });
     subitems[i].classList.add("active");
-    // if(subitems[i].style.display == 'flex') {
-    //   //subitems[i].style.display = 'none';
-    //   subitems[i].classList.remove('active');
-    //   console.log("Desativou " + subitems[i]);
-    //   items[i].style.color = 'black';
-    // }
-    // else{
-    //   //subitems[i].style.display = 'flex';
-    //   subitems[i].classList.add('active');
-    //   console.log("Ativou " + subitems[i]);
-    //   document.querySelectorAll('.active').forEach(function(item){
-    //     item.classList.remove('active');
-    //     console.log("Desativou " + item);
-    //   });
-    //   items[i].style.color = 'rgba(52, 73, 94,0.8)';
-    // }
   });
 }
 const button = document.querySelectorAll('.ButtonSeeMore');
