9b2e819d71d949b1cc84cca25caf9544386ab89a
Marlon Furtado <marlonfurtado.rs@gmail.com>
2017-06-09 10:54:12 -0300

    #9<@marlonfurtado,@tatianeaguirres>-Criar botao enviar para modal

--- public/js/modal_contact.js
+++ public/js/modal_contact.js
@@ -1,16 +1,23 @@
 var modal = document.querySelector('#ModalContact');
+var company = document.querySelector('#SendEmail');
 
-function showModal() {
+function showModal(id) {
   modal.style.display = "flex";
+
+  company.id = "SendEmail"+id;
+
 }
 
 function closeModal() {
   modal.style.display = "none";
+
+  company.id = "SendEmail";
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    company.id = "SendEmail";
   }
 }
 
