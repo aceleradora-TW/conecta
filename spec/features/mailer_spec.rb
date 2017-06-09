bdaf1b60b8fbe130a682a9055b9b43e5cd546144
Bernardo Felipetto <be.felipetto@hotmail.com>
2017-06-09 11:00:11 -0300

    #9<@mvoliveira,@gbrlcardozo>-Teste para notification mailer.

---
+++ spec/features/mailer_spec.rb
@@ -0,0 +1,20 @@
+require 'rack/test'
+require 'rspec'
+require 'spec_helper'
+
+
+RSpec.describe Mailer, type: :mailer do
+
+  before(:all) do
+    @mail = Mailer.notification().deliver
+  end
+  describe "#notification" do
+    context "Quando chamar o metodo" do
+
+      it "Deve enviar um e-mail para cvcardozo@me.com" do
+        expect(@mail.to).to eq(['xxx@xxx.com'])
+      end
+
+    end
+  end
+end
