20e00ca20a6d0063191c50e61806dbc872dd7c44
Anna Leme <anna_leme@hotmail.com>
2017-06-12 15:25:22 -0300

    #0<@annaleme,@souzabruna>-Criar navbar responsivo para tablet

---
+++ public/js/navbar.js
@@ -0,0 +1,14 @@
+var hamburger = document.querySelector('.Hamburguer');
+var navLinks =  document.querySelector('.NavLinks');
+var Navbar = document.querySelector('.Navbar');
+hamburger.addEventListener("click", function() {
+  if (navLinks.style.display == "none" || navLinks.style.display === "") {
+    navLinks.style.display = "flex";
+    Navbar.style.background = "rgba(0,0,0,0.55)";
+    Navbar.style.height = "37vh";
+  } else {
+    navLinks.style.display = "none";
+    Navbar.style.background = "rgba(0,0,0,0.47)";
+    Navbar.style.height = "15vh";
+  }
+});
