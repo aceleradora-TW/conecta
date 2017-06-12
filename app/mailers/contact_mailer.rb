fbd17104b4c04e184a06614f4ac461b37ee1318b
Gabriela Cardozo <gabrielacardozo09@gmail.com>
2017-06-12 15:58:12 -0300

    #9<@gmeneguz,@gbrlcardozo>-Criar modelagem e funcionalidade de email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,16 +1,12 @@
-require 'action_mailer'
-ActionMailer::Base.view_paths = File.expand_path('../../../app/views/', __FILE__)
 
-class Mailer < ActionMailer::Base
-  default to: "aceleradora11@gmail.com"
-  default from: "aceleradora11@gmail.com"
-
-  def send_email_contact
-    mail(
-      to: "aceleradora11@gmail.com", :from    => "aceleradora11@gmail.com", :subject => "Dale",
-      :template_path => "app/views/mailer", template_name: 'notification') do |format|
-      format.html { render 'notification' }
-      format.text { render plain: 'notification' }
-    end
+class ContactMailer
+  attr_accessor :email
+  def initialize email_data
+    email_data.subject = "Email de contato"
+    @mailer = Mailer.new
+    @email = @mailer.create_email(email_data,"app/views/mailer","notification")
+  end
+  def send_now
+    @email.deliver
   end
 end
--- spec/features/mailer_spec.rb
+++ spec/features/mailer_spec.rb
@@ -4,17 +4,17 @@ require 'spec_helper'
 
 
 RSpec.describe Mailer, type: :mailer do
-
   before(:all) do
-    @contact_mail = Mailer.send_email_contact()
+    #@configs = {from: {name: "User1", email: , comment: },to:{ name: , email: },subject: "Test Mail"}
+    @email_info = EmailData.new("User1", "aceleradora11@gmail.com", "Olá Gabi!", "Gabriela", "gabrielacardozo09@gmail.com","Test Mail")
+    @contact_mailer = ContactMailer.new @email_info
   end
   describe "#send_email_contact" do
     context "Quando chamar o metodo" do
-
-      it "Deve enviar um e-mail para aceleradora11@gmail.com" do
-        expect(@contact_mail.to).to eq(['aceleradora11@gmail.com'])
+      it "Deve enviar um e-mail para Usuario" do
+        @contact_mailer.send_now
+        expect(@contact_mailer.email.to).to eq(["gabrielacardozo09@gmail.com"])
       end
-
     end
   end
 end
