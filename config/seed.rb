656f415c7cf9c02abfe941a3b588f3a82bf5de69
Bernardo Felipetto <be.felipetto@hotmail.com>
2017-06-19 15:17:43 -0300

    #10<@gmeneguz,@bernardofelipetto>-Adicionar Seed para usuarios

--- config/seed.rb
+++ config/seed.rb
@@ -7,4 +7,8 @@ class Seed
     ResearchCenter.create(name: "Fake Research Center 1")
     ResearchCenter.create(name: "Fake Research Center 2")
   end
+  def self.seed_users
+    User.create(name: 'admin', email: 'admin@admin.com', password: 'admin', role: 'admin')
+    User.create(name: 'user', email: 'user@user.com', password: 'user', role: 'user')
+  end
 end
