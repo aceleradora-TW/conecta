70271f87a915a94e83cbaf300cae2ed83eb79f0f
Anna Leme <anna_leme@hotmail.com>
2017-06-21 13:19:03 -0300

    #0<@annaleme,@souzabruna>-Fazer as alterações do code review no navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,14 +1,6 @@
-var hamburger = document.querySelector('.Hamburger');
-var navLinks =  document.querySelector('.NavLinks');
-var Navbar = document.querySelector('.Navbar');
-hamburger.addEventListener("click", function() {
-  if (navLinks.style.display == "none" || navLinks.style.display === "") {
-    navLinks.style.display = "flex";
-    Navbar.style.background = "rgba(0,0,0,0.55)";
-    Navbar.style.height = "37vh";
-  } else {
-    navLinks.style.display = "none";
-    Navbar.style.background = "rgba(0,0,0,0.47)";
-    Navbar.style.height = "15vh";
-  }
-});
+function navToggle() {
+  var Navbar = document.querySelector('.Navbar');
+  var navLinks = document.querySelector('.NavLinks');
+  Navbar.classList.toggle("is-open");
+  navLinks.classList.toggle("is-open");
+}
