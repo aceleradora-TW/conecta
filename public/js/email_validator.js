435edbbc9ad048ad903f79c1843723c95fdc777a
Fernanda Almeida <fe.shakti@hotmail.com>
2017-06-23 15:54:39 -0300

    #9<@fealmeida,@leoalmig>-Usar regex para validar email

--- public/js/email_validator.js
+++ public/js/email_validator.js
@@ -1,5 +1,6 @@
 function isValidEmail(email) {
-  return true;
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 };
 
 // Para rodar os testes
