82f847dcb0485fa97edb30582d8071e047fed7af
Fernanda Almeida <fe.shakti@hotmail.com>
2017-06-23 15:54:39 -0300

    #9<@fealmeida,@leoalmig>-Usar regex para validar email

--- spec/js/email_validator_test.js
+++ spec/js/email_validator_test.js
@@ -5,6 +5,13 @@ describe('EmailValidator', function() {
   describe('#isValidEmail', function() {
     it('retorna true quando email é válido', function() {
       expect(emailValidator.isValidEmail("aceleradora@gmail.com")).to.equal(true);
+      expect(emailValidator.isValidEmail("aceleradora@gmail.com.br")).to.equal(true);
+      expect(emailValidator.isValidEmail("aceleradora11@pucrs.br")).to.equal(true);
+    });
+
+    it('retorna falso quando email for inválido', function() {
+      expect(emailValidator.isValidEmail("aceleradoragmail.com")).to.equal(false);
+      expect(emailValidator.isValidEmail("aceleradora@.com")).to.equal(false);
     });
   });
 });
