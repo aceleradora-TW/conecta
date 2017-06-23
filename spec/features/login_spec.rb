fe42aec03ed20252f1db420d5deb5d529141d4e9
Henrique Sangalli <habyte@gmail.com>
2017-06-23 15:48:27 -0300

    #0<@gmeneguz>-Fazer testes capybara

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,23 +5,24 @@ require 'spec_helper'
 RSpec.describe IndexController do
   describe "Index" do
     context "Quando abrir página de login" do
-      it "Deve mostrar o titulo Login" do
+
+      before :each do
         visit "/admin"
+      end
+
+      it "Deve mostrar o titulo Login" do
         expect(page).to have_content 'Login'
       end
 
       it "Deve exibir os campos de texto do Login" do
-        visit "/admin"
         expect(page).to have_css("input", class: 'InputLogin')
       end
 
       it "Deve exibir o botão de Login" do
-        visit "/admin"
         expect(page).to have_css("button", class: 'ButtonSend')
       end
 
       it "Deve mostrar link 'esqueceu sua senha?'" do
-        visit "/admin"
         expect(page).to have_link('Esqueceu sua senha?')
       end
     end
