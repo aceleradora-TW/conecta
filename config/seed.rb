afdb70ff37020982d4f48b3dbcbe74f28cb7a3e0
Henrique Sangalli <habyte@gmail.com>
2017-06-23 15:27:47 -0300

    #0<@gmeneguz>-Arrumar o bug do capybara

--- config/seed.rb
+++ config/seed.rb
@@ -4,8 +4,8 @@ class Seed
     Company.create(name: "Eldorado")
     Company.create(name: "HPServ")
 
-    ResearchCenter.create(name: "Laboratório de Tratamento de imagens e Geoprocessamento")
-    ResearchCenter.create(name: "Laboratório de Sistema Wireless")
+    ResearchCenter.create(name: "Centro de Pesquisas em Biologia Molecular e Funcional")
+    ResearchCenter.create(name: "Centro de Terapia Celular")
   end
   def self.seed_users
     User.create(name: 'admin', email: 'admin@admin.com', password: 'admin', role: 'admin')
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,22 +5,22 @@ require 'spec_helper'
 RSpec.describe IndexController do
   describe "Index" do
     context "Quando abrir página de login" do
-      skip "Deve mostrar o titulo Login" do
+      it "Deve mostrar o titulo Login" do
         visit "/admin"
         expect(page).to have_content 'Login'
       end
 
-      skip "Deve exibir os campos de texto do Login" do
+      it "Deve exibir os campos de texto do Login" do
         visit "/admin"
-        skip expect(page).to have_css("input", class: 'InputLogin')
+        expect(page).to have_css("input", class: 'InputLogin')
       end
 
-      skip "Deve exibir o botão de Login" do
+      it "Deve exibir o botão de Login" do
         visit "/admin"
         expect(page).to have_css("button", class: 'ButtonSend')
       end
 
-      skip "Deve mostrar link 'esqueceu sua senha?'" do
+      it "Deve mostrar link 'esqueceu sua senha?'" do
         visit "/admin"
         expect(page).to have_link('Esqueceu sua senha?')
       end
