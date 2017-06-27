80d8deba92cef1ea9b09d5c33484cdf3a9e50820
Fernanda Almeida <fe.shakti@hotmail.com>
2017-06-27 11:10:36 -0300

    #0<@fealmeida>-Testar retorno vazio de instituições

--- spec/unit/router_service_spec.rb
+++ spec/unit/router_service_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../../app/services/router_service'
 
 describe RouterService do
-  it "Retorna competencias procuradas" do
+  it "Chama o serviço que retorna competencias" do
     mock_search_service = spy("mock_search_service")
     router_service = RouterService.new(mock_search_service)
 
@@ -12,11 +12,25 @@ describe RouterService do
 
   end
 
+  it "Não retorna instituição quando competencia não é especificada" do
+    mock_search_service = double("mock_search_service")
+    allow(mock_search_service).to receive(:find_by_competence).and_return([])
+    router_service = RouterService.new(mock_search_service)
+
+    result = router_service.return_competence("dummy", "dummy")
+
+    expect(result).to eq([])
+  end
 end
 
 
 
 
+
+# it "Chama o serviço que retorna Empresas relacionadas às competências" do
+#
+#
+# end
 
 
 
