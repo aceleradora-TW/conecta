85f6f14147cb47cd7d4879029651afbd1be82636
Bernardo Felipetto <be.felipetto@hotmail.com>
2017-06-28 09:43:36 -0300

    #0<@bernardofelipetto>-dar push em teste que nao me pertence para poder trabalhar em outra branch

--- spec/unit/router_service_spec.rb
+++ spec/unit/router_service_spec.rb
@@ -2,6 +2,16 @@ require 'rspec'
 require_relative '../../app/services/router_service'
 
 describe RouterService do
+  it "Não retorna instituição quando competencia não é especificada" do
+    mock_search_service = double("mock_search_service")
+    allow(mock_search_service).to receive(:find_by_competence).and_return([])
+    router_service = RouterService.new(mock_search_service)
+
+    result = router_service.return_competence("dummy", "dummy")
+
+    expect(result).to eq([])
+  end
+
   it "Chama o serviço que retorna competencias" do
     mock_search_service = spy("mock_search_service")
     router_service = RouterService.new(mock_search_service)
@@ -12,25 +22,30 @@ describe RouterService do
 
   end
 
-  it "Não retorna instituição quando competencia não é especificada" do
+  skip "Chama o serviço que retorna empresas relacionadas às competências" do
     mock_search_service = double("mock_search_service")
-    allow(mock_search_service).to receive(:find_by_competence).and_return([])
+
+    # mock_institution = double ("mock_institution")
+
+
+
+    allow(mock_search_service).to receive(:find_by_competence).and_return(["software", "hardware"])
+    allow(mock_search_service).to receive(:find_related_competence_institutions).and_return(competence_institution)
     router_service = RouterService.new(mock_search_service)
 
-    result = router_service.return_competence("dummy", "dummy")
 
-    expect(result).to eq([])
+    result = router_service.return_competence("Dell","company")
+    expect(result).to eq("Dell")
   end
+
+
+
 end
 
 
 
 
 
-# it "Chama o serviço que retorna Empresas relacionadas às competências" do
-#
-#
-# end
 
 
 
