a57630b820511e66547387a671fb4fd4901a2647
Fernanda Almeida <fe.shakti@hotmail.com>
2017-06-29 15:35:54 -0300

    #0<@fealmeida,@filipehassen>-Remover Initialize

--- public/js/change_on_scroll.js
+++ public/js/change_on_scroll.js
@@ -1,17 +1,11 @@
-var initialize = function(){
-  changeOnScroll();
-};
-
 var verifyScrolled = function(scrolled){
   var navbar = document.querySelector('.Navbar');
-  console.log(navbar);
   if (scrolled){
     navbar.classList.add('is-scrolling');
   }else{
     navbar.classList.remove('is-scrolling');
   }
 };
-
 
 var changeOnScroll = function(){
   window.addEventListener('scroll', function(){
@@ -24,4 +18,4 @@ var changeOnScroll = function(){
   });
 };
 
-initialize();
+changeOnScroll();
