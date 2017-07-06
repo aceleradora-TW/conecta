79468fa6bbc8115d3c3fae39a09654b3bcf695b5
Gianfranco Meneguz <gmeneguz@gmail.com>
2017-07-06 15:49:31 -0300

    #0<@gmeneguz,@mvoliveira>-Refatorar transition do filtro

--- public/js/search_filter.js
+++ public/js/search_filter.js
@@ -6,13 +6,6 @@ var filterSearch = document.querySelectorAll('label');
 var filterDescription = document.querySelector('#ReplaceFilter');
 var inputSearch = document.querySelector('#InputSearch');
 
-window.onload = function onLoad(){
-  var urlHasAnchor = location.href.indexOf("#") > 0;
-  if(!urlHasAnchor){
-    document.getElementById("InputSearch").focus();
-  }
-}
-
 if(inputSearch){
   inputSearch.addEventListener('focus', function(e){
     menu.classList.add('DropdownMenu-visible');
