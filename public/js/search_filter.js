51f37334635e54761ccc1fbb71d1b2bdb53f971a
Bernardo Felipetto <be.felipetto@hotmail.com>
2017-07-07 14:19:45 -0300

    #0<@gmeneguz,@bernardofelipetto>-adicionar popup no botao para competencia nao selecionada

--- public/js/search_filter.js
+++ public/js/search_filter.js
@@ -5,11 +5,39 @@ var textFilter = document.querySelector('.text-filter');
 var filterSearch = document.querySelectorAll('label');
 var filterDescription = document.querySelector('#ReplaceFilter');
 var inputSearch = document.querySelector('#InputSearch');
+var tooltip = document.querySelector('#tooltipMessage');
 
 if(inputSearch){
-  inputSearch.addEventListener('focus', function(e){
+  inputSearch.addEventListener('focus', function(event){
     menu.classList.add('DropdownMenu-visible');
     console.log('focus');
   })
 
+}
+
+function validateFilter(){
+  var requiredFilterInputs = document.querySelectorAll('.requiredFilter');
+  var selectedFilter = false;
+
+  tooltip.classList.remove('Tooltiptext-active');
+  requiredFilterInputs.forEach(function(requiredInput){
+    if(requiredInput.checked){
+      selectedFilter = true;
+    }
+    requiredInput.onchange = function(event){
+
+      if (this.checked){
+        tooltip.classList.remove('Tooltiptext-active');
+
+      }
+    };
+  });
+
+  if(selectedFilter){
+    document.forms['search'].submit();
+    return true;
+  }else{
+    tooltip.classList.add('Tooltiptext-active');
+  }
+  return false;
 }
