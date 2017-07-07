a68b63f71921507b9cd6f28a49ac88cb2875f56f
Henrique Sangalli <habyte@gmail.com>
2017-07-07 14:42:27 -0300

    #0<@gbrlcardozo,@hsangalli>-Refatorar JavaScript do filtro de busca

--- public/js/search_filter.js
+++ public/js/search_filter.js
@@ -1,58 +1,41 @@
-var btnFilter = document.querySelector('.ButtonFilter');
-var menu = document.querySelector('.DropdownMenu');
-var textFilter = document.querySelector('.text-filter');
-var filterDescription = document.querySelector('#ReplaceFilter');
+const dropdownMenu = document.querySelector('.DropdownMenu');
+const inputSearch = document.querySelector('#InputSearch');
+const searchFilterElements = [inputSearch, dropdownMenu];
+const tooltip = document.querySelector('#tooltipMessage');
 
-var filterSpans = document.querySelectorAll('span.u-fontBold.u-marginBottom');
-var filterLabels = document.querySelectorAll('.filterLabel');
-var inputSearch = document.querySelector('#InputSearch');
-
-var tooltip = document.querySelector('#tooltipMessage');
-
-function validateFilter(){
-  var requiredFilterInputs = document.querySelectorAll('.requiredFilter');
-  var selectedFilter = false;
-
-  tooltip.classList.remove('Tooltiptext-active');
-  requiredFilterInputs.forEach(function(requiredInput){
-    if(requiredInput.checked){
-      selectedFilter = true;
-    }
-    requiredInput.onchange = function(event){
-
-      if (this.checked){
-        tooltip.classList.remove('Tooltiptext-active');
-
-      }
-    };
+searchFilterElements.forEach(function(element){
+  element.addEventListener('click', function(event){
+    event.stopPropagation();
   });
-
-  if(selectedFilter){
-    document.forms['search'].submit();
-    return true;
-  }else{
-    tooltip.classList.add('Tooltiptext-active');
-  }
-  return false;
-}
-
-var filterRadios = document.querySelector('input[type="radio"]');
-var filterDiv = document.querySelector('.DropdownMenu div');
-
+});
 
 inputSearch.addEventListener('focus', function(event){
-  menu.classList.add('DropdownMenu-visible');
-  event.stopPropagation();
-});
-
-inputSearch.addEventListener('click', function(event){
-  event.stopPropagation();
-});
-
-menu.addEventListener('click', function(event){
+  dropdownMenu.classList.add('DropdownMenu-visible');
   event.stopPropagation();
 });
 
 document.addEventListener('click', function(){
-  menu.classList.remove('DropdownMenu-visible');
+  dropdownMenu.classList.remove('DropdownMenu-visible');
 });
+
+function validateFilter(){
+  const requiredFilterInputs = document.querySelectorAll('.requiredFilter');
+  var filterWasSelected = false;
+
+  tooltip.classList.remove('Tooltiptext-active');
+  requiredFilterInputs.forEach(function(requiredInput){
+    if(requiredInput.checked) filterWasSelected = true;
+    requiredInput.onchange = function(event){
+      if (this.checked) tooltip.classList.remove('Tooltiptext-active');
+    };
+  });
+
+  if(filterWasSelected){
+    document.forms['search'].submit();
+    return true;
+  } else{
+    tooltip.classList.add('Tooltiptext-active');
+  }
+
+  return false;
+}
