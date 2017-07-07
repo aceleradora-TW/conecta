186659b865ce82040e9813c5e0d7faed42463e5f
Gabriela Cardozo <gabrielacardozo09@gmail.com>
2017-07-07 14:15:18 -0300

    #0<@gbrlcardozo,@hsangalli>-Fechar filtro quando clicar fora

--- public/js/search_filter.js
+++ public/js/search_filter.js
@@ -1,15 +1,28 @@
-
 var btnFilter = document.querySelector('.ButtonFilter');
 var menu = document.querySelector('.DropdownMenu');
 var textFilter = document.querySelector('.text-filter');
-var filterSearch = document.querySelectorAll('label');
 var filterDescription = document.querySelector('#ReplaceFilter');
+
+var filterSpans = document.querySelectorAll('span.u-fontBold.u-marginBottom');
+var filterLabels = document.querySelectorAll('.filterLabel');
 var inputSearch = document.querySelector('#InputSearch');
+var filterRadios = document.querySelector('input[type="radio"]');
+var filterDiv = document.querySelector('.DropdownMenu div');
 
-if(inputSearch){
-  inputSearch.addEventListener('focus', function(e){
-    menu.classList.add('DropdownMenu-visible');
-    console.log('focus');
-  })
 
-}
+inputSearch.addEventListener('focus', function(event){
+  menu.classList.add('DropdownMenu-visible');
+  event.stopPropagation();
+});
+
+inputSearch.addEventListener('click', function(event){
+  event.stopPropagation();
+});
+
+menu.addEventListener('click', function(event){
+  event.stopPropagation();
+});
+
+document.addEventListener('click', function(){
+  menu.classList.remove('DropdownMenu-visible');
+});
